a05016e97d61813e8b308f13b68b989b9a4197fb
tungngo2525 <tungngo2525@gmail.com>
2024-09-23 20:42:54 +0700

    init for Ex1

--- LAB1/Core/Inc/main.h
+++ LAB1/Core/Inc/main.h
@@ -58,6 +58,10 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define RED_LED_Pin GPIO_PIN_5
+#define RED_LED_GPIO_Port GPIOA
+#define GREEN_LED_Pin GPIO_PIN_6
+#define GREEN_LED_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
